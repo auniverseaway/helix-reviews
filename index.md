36b96da4d6e338d496650374e723433841fdeb2a
Chris Millar <cmillar@adobe.com>
2020-12-19 11:05:48 -0700

    Its working!

--- index.md
+++ index.md
@@ -1,1 +1,19 @@
-# Welcome to reviews
+<h1>Rate Me</h1>
+<form data-sheet="https://adobe-my.sharepoint.com/:x:/r/personal/cmillar_adobe_com/Documents/Helix%20Demos/dc-rating-submissions.xlsx?d=w9d7427d7a9814408846078023a361d06&csf=1&web=1&e=GkX4Kc">
+  <fieldset>
+      <input type="radio" name="rating" value="1" />
+      <label for="rating-1">1</label>
+      <input type="radio" name="rating" value="2" />
+      <label for="rating-2">2</label>
+      <input type="radio" name="rating" value="3" />
+      <label for="rating-3">3</label>
+      <input type="radio" name="rating" value="4" />
+      <label for="rating-4">4</label>
+      <input type="radio" name="rating" value="5" />
+      <label for="rating-5">5</label>
+  </fieldset>
+  <fieldset>
+    <label for="rating-comments">Leave a comment</label>
+    <textarea id="rating-comments" name="rating-comments" rows="4" cols="50"></textarea>
+  </fieldset>
+</form>
--- scripts.js
+++ scripts.js
@@ -1,1 +1,60 @@
-console.log('hello world');
+const locale = 'us-EN';
+
+const postURL = 'https://ccgrowth.servicebus.windows.net/formsink/messages';
+const postAuth = 'SharedAccessSignature sr=https%3A%2F%2Fccgrowth.servicebus.windows.net%2Fformsink%2Fmessages&sig=RFndMU%2FyHZrlchNBfHlIdulld4URAgUAQdAlqVLf1Bw%3D&se=1634259041&skn=send';
+const testURL = 'https://adobeioruntime.net/api/v1/web/helix-clients/ccgrowth/forms-handler@v1';
+
+const getDate = () => {
+    return new Date().toISOString().replace(/[TZ]/g, ' ').split('.')[0].trim();
+};
+
+const getForm = (el) => {
+    return el.closest('form');
+}
+
+const sendRequest = (target) => {
+    const form = getForm(target);
+    const { sheet } = form.dataset;
+
+    const data = [
+        { name: 'timestamp', value: getDate() },
+        { name: 'Rating', value: target.value },
+        { name: 'Locale', value: locale }
+    ];
+
+    const body = { sheet, data };
+
+    fetch(postURL, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': postAuth,
+        },
+        body: JSON.stringify(body),
+    });
+};
+
+const showComments = (radio) => {
+    const form = radio.closest('form');
+    const textarea = form.querySelector('textarea');
+    textarea.classList.add('is-Visible');
+};
+
+const radioClicked = (e) => {
+    const { value } = e.target;
+    if (value <= 2) {
+        showComments(e.target);
+    } else {
+        sendRequest(e.target);
+    }
+    e.target.classList.add('is-Clicked');
+};
+
+const setupRating = () => {
+    const rateRadios = document.querySelectorAll('fieldset > input[type="radio"]');
+    rateRadios.forEach((radio) => {
+        radio.addEventListener("click", radioClicked);
+    });
+};
+
+setupRating();
